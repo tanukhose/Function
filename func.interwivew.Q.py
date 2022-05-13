e9311da8550990d325d8dff4723162efdd46734f
tanukhose <104750176+tanukhose@users.noreply.github.com>
2022-05-13 20:18:14 +0530

    Add files via upload

---
+++ func.interwivew.Q.py
@@ -0,0 +1,28 @@
+# def fact(num):
+#     if num==1:
+#         return 1
+#     return num*fact(num-1)
+# print(fact(5))
+
+
+
+# def a():
+#     i=1
+#     while i<=5:
+#         j=5
+#         while j>=i:
+#             print("_",end="")
+#             j-=1
+#         print()
+#         i+=1
+#     def b():
+#         i=1
+#         while i<=5:
+#             j=1
+#             while j<=i:
+#                 print("_",end="")
+#                 j+=1
+#             print()
+#             i+=1
+#     b()
+# a()
